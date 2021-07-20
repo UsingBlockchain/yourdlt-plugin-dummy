df1ed2ee561c397a742c053c8130e538fffe407e
Gregory Saive <greg@evias.be>
2021-07-20 16:46:46 +0200

    Example Plugin for YourDLT / dHealth / Symbol Wallets

---
+++ src/Dummy.ts
@@ -0,0 +1,62 @@
+/**
+ * This file is part of YourDLT Wallet Plugins shared under LGPL-3.0
+ * Copyright (C) 2021 Using Blockchain Ltd, Reg No.: 12658136, United Kingdom
+ *
+ * @package     YourDLT Wallet Plugins
+ * @subpackage  @yourdlt/plugin-dummy
+ * @author      Grégory Saive for Using Blockchain Ltd <greg@ubc.digital>
+ * @license     LGPL-3.0
+ */
+import Vue, { VueConstructor } from "vue";
+
+// internal dependencies
+// child components
+import Dummy from "./views/Dummy/Dummy.vue";
+
+/// region components library
+const components: { [s: string]: VueConstructor } = {
+  Dummy,
+};
+
+export const registerComponents = (): { [s: string]: VueConstructor } => {
+  Object.keys(components).forEach((k) => Vue.component(k, components[k]));
+  return components;
+};
+/// end-region components library
+
+/// region installable plugin
+/**
+ * @export default
+ * @description This plugin is an example software repository that
+ * serves as an installable dummy plugin. The components published
+ * only display placeholder texts.
+ * @version 0.1.0
+ * @license LGPL-3.0
+ */
+export default {
+  view: "Dummy",
+
+  routes: [
+    {
+      path: "/dummy",
+      name: "dummy.home",
+      meta: {
+        protected: true,
+        title: "Example Home",
+        hideFromMenu: true,
+      },
+      // @ts-ignore
+      component: () => import("@/views/Dummy/Dummy.vue"),
+      props: false,
+    },
+  ],
+
+  components,
+
+  storages: [],
+
+  settings: [],
+
+  permissions: []
+};
+/// end-region installable plugin
---
+++ src/views/Dummy/Dummy.vue
@@ -0,0 +1,57 @@
+<!--
+/**
+ * This file is part of YourDLT Wallet Plugins shared under LGPL-3.0
+ * Copyright (C) 2021 Using Blockchain Ltd, Reg No.: 12658136, United Kingdom
+ *
+ * @package     YourDLT Wallet Plugins
+ * @subpackage  @yourdlt/plugin-dummy
+ * @author      Grégory Saive for Using Blockchain Ltd <greg@ubc.digital>
+ * @license     LGPL-3.0
+ */
+-->
+<template>
+  <div class="yourdlt-plugin-dummy-container">
+    <div class="dashboard-left-container">
+      <div class="title">
+        <span class="title_txt">Example plugin</span>
+      </div>
+      <div class="history-wrapper">
+        <h2>This plugin serves as an example for the development of custom plugins.</h2>
+      </div>
+    </div>
+    <div class="dashboard-right-container">
+      <div class="title">
+        <span class="title_txt">Plugin details</span>
+      </div>
+      <p>This software serves as an example custom plugin that can be installed in YourDLT / dHealth / Symbol Wallets.</p>
+      <a
+        class="github-fork-ribbon right-bottom"
+        :href="forkUrl"
+        target="_blank"
+        data-ribbon="Fork me on GitHub"
+        title="Fork me on GitHub">Fork me on GitHub</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Dummy extends Vue {
+
+  @Prop({ default: 'https://github.com/UsingBlockchain/yourdlt-plugin-dummy' })
+  public forkUrl: string;
+
+  /// region component methods
+  public async created() {
+    console.log(`Dummy created`);
+  }
+  /// end-region component methods
+}
+</script>
+
+<style lang="less" scoped>
+@import "./Dummy.less";
+@import "./ForkMe.less";
+</style>
